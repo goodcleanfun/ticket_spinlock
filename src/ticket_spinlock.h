da5f85e7beb888a822404761eb78ee591a2d60e0
Al <albarrentine@gmail.com>
2025-08-01 04:30:52 -0400

    [types] using uint_fast16_t throughout

--- src/ticket_spinlock.h
+++ src/ticket_spinlock.h
@@ -20,7 +20,7 @@ static inline void ticket_spinlock_init(ticket_spinlock_t *lock) {
 }
 
 static void ticket_spinlock_lock(ticket_spinlock_t *lock) {
-    uint16_t ticket = atomic_fetch_add_explicit(&lock->ticket, 1, memory_order_acq_rel);
+    uint_fast16_t ticket = atomic_fetch_add_explicit(&lock->ticket, 1, memory_order_acq_rel);
 
     for (size_t i = 0; i < MAX_PAUSE_ITERATIONS; i++) {
         if (atomic_load_explicit(&lock->now_serving, memory_order_acquire) == ticket) return;
@@ -36,18 +36,18 @@ static void ticket_spinlock_unlock(ticket_spinlock_t *lock) {
 }
 
 static bool ticket_spinlock_trylock(ticket_spinlock_t *lock) {
-    uint16_t ticket = atomic_load_explicit(&lock->ticket, memory_order_relaxed);
-    uint16_t now_serving = atomic_load_explicit(&lock->now_serving, memory_order_relaxed);
+    uint_fast16_t ticket = atomic_load_explicit(&lock->ticket, memory_order_relaxed);
+    uint_fast16_t now_serving = atomic_load_explicit(&lock->now_serving, memory_order_relaxed);
     if (ticket != now_serving) return false;
 
-    uint16_t next_ticket = ticket + 1;
+    uint_fast16_t next_ticket = ticket + 1;
 
     return atomic_compare_exchange_strong(&lock->ticket, &ticket, next_ticket);
 }
 
 static bool ticket_spinlock_is_locked(ticket_spinlock_t *lock) {
-    uint16_t ticket = atomic_load(&lock->ticket);
-    uint16_t now_serving = atomic_load(&lock->now_serving);
+    uint_fast16_t ticket = atomic_load(&lock->ticket);
+    uint_fast16_t now_serving = atomic_load(&lock->now_serving);
     return now_serving != ticket;
 }
 
